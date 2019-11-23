97c2f9e4b9da437a94c1a30e5b632a17c31e2c8e
Kaizu Shibata <kaizushi@volatile.ch>
2019-11-23 04:20:40 +0000

    There were some issues with the template.php file

--- template.php
+++ template.php
@@ -26,3 +26,7 @@ table {
 <h1><?php printTitle(); ?></h1>
 <?php printPageBody(); ?>
 </tr></td></table>
+<br><br><br><br><br><br>
+<p><?php echo date("Y") . " "; echo SITEAUTHOR; echo ", server time " . date("G:i:s d/m/y") . " UTC"; ?>
+<p><i>Powered by Kaizu's <a href="https://github.com/kaizushi/picosite"><?php echo SOFTNAME; ?></a>!</i></p>
+</body></html>
