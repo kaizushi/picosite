f0bb3bce3c51b8e10ca43066201e5e47d59036c4
Kaizu Shibata <kaizushi@volatile.ch>
2020-03-31 19:56:45 +0000

    This has been tested and seems to work.

--- hostcheck.php
+++ hostcheck.php
@@ -1,20 +1,35 @@
 <?php
 
+// THIS IS NOT FINISHED BUT IS UNIT-TESTED TO BE A GOOD START
+
 // This is a service checker which will write to 'data' in the
 // site directory about what services are online. This data
 // is for picosite to then display this information.
 
-define("SVCLIST", "/home/kaizushi/servicelist.txt");
-define("SVCUP", "/var/www/user/kaizushi/kaizushigdv5mrnz.onion/data/svcup.txt");
+$_config_hc_srvclist = "servicelist.txt";
+$_config_hc_srvcdata = "data/";
+$_config_hc_redirlim = 5;
 
-function getServices() {
-	if (!file_exists(SVCLIST)) return [];
-	$services = file_get_contents(SVCLIST);
-	return explode("\n", $services);
+if (file_exists("settings.php")) include_once("settings.php");
+
+define("SVCLIST", "$_config_srvclist");
+define("SVCDATA", "$_config_srvcdata");
+
+global $redir_count;
+$redir_count = 0;
+
+function transEnd($instring, $substring) {
+        $length = strlen($substring);
+        return $length === 0 || (substr($instring, -$length) === $substring);
+}
+
+function checkURL($url) {
+	return filter_var($url, FILTER_VALIDATE_URL);
 }
 
 function checkServices($services) {
 	$online = [];
+	$elsewhere = [];
 	foreach ($services as $svc) {
 		$curl = curl_init($svc);
 		curl_setopt($curl, CURLOPT_HEADER, true);
@@ -23,14 +38,24 @@ function checkServices($services) {
 		curl_setopt($curl, CURLOPT_TIMEOUT,10);
 		$output = curl_exec($curl);
 		$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		echo "Debug: HTTP code: $httpcode type " . gettype($httpcode) . "\n";
-		if ($httpcode === "200") {
-			$online.array_push($svc);
+		if ($httpcode === 200 || $httpcode === 302) array_push($online, $svc);
+		if ($httpcode === 301) {
+			$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+			$headers = substr($output, 0, $header_size);
+			$headers = explode("\n", $headers);
+			foreach ($headers as $header) {
+				$headparts = explode(":", $header);
+				if ($headparts[0] === "Location" && count($headparts) === 3) {
+					$url = trim($headparts[1] . ":" . $headparts[2]);
+					if (!checkURL($url)) continue;
+					array_push($elsewhere, $url);
+				}
+			}
 		}
 	}
+	
+	if (!empty($elsewhere)) array_merge($online, checkServices($elsewhere));
 
-	return $online;
+	return array_unique($online);
 }
 
-$services = ["http://kaizushigdv5mrnz.onion", "http://dhfndjfoenclduie.onion", "http://kaizushih5iec2mxohpvbt5uaapqdnbluaasa2cmsrrjtwrbx46cnaid.onion/"];
-checkServices($services);
